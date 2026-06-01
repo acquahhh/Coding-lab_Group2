03100d1a6f36ddf624f37976fe43f30676602e6a
acquahhh <e.adomanu@alustudent.com>
2026-06-01 08:36:10 -0700

    M5: Add awk to extract fields and save to critical_alerts.txt

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -6,6 +6,6 @@ process_vitals() {
         awk -F',' '{printf "%-24s %-22s %s\n", $1,$2,$3}' >> reports/critical_alerts.txt
     grep "CRITICAL" active_logs/temperature.log | \
         awk -F',' '{printf "%-24s %-22s %s\n", $1,$2,$3}' >> reports/critical_alerts.txt
-    echo "Critical alerts saved to reports/critical_alerts.txt"
+    echo "Critical alerts saved to reports/critical_alerts.txt "
 }
 
