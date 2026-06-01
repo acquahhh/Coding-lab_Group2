4281c18436140e91e5698fc9bd6637f96f7e1df8
mwayo-tech <m.wayo@alustudent.com>
2026-06-01 08:27:35 -0700

    comment changes

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -21,12 +21,12 @@ secure_data() {
 	chmod 700 active_logs
 	ls -ld active_logs
 }
-// The echo command is to let you know that permissions will be changed to secure active logs.
-//The chmod changes the permissons on the active_log file.
+#The echo command is to let you know that permissions will be changed to secure active logs.
+#The chmod changes the permissons on the active_log file.
 
 
 initialize_system
 secure_data
 echo "Security Environment Secured"
 date
-// This scrip lets you know that the environment is secure.
+#This scrip lets you know that the environment is secure.
