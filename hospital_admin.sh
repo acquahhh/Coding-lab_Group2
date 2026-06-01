199d6b6820587f1350f4e666a200f7c5ea89f552
mwayo-tech <m.wayo@alustudent.com>
2026-06-01 07:36:28 -0700

    Ready

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -6,3 +6,9 @@ secure_data() {
 }
 // The echo command is to let you know that permissions will be changed to secure active logs.
 //The chmod changes the permissons on the active_log file.
+
+
+initialize_system
+secure_data
+echo "Security Environment Secured"
+date
